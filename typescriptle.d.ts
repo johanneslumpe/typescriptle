a08d78b300ef72ae39a04f63e17fc956f5382708
Johannes Lumpe <johannes@johanneslumpe.de>
2022-03-26 13:33:24 -0400

    implement correct lookahead/lookbehind for matches

--- typescriptle.d.ts
+++ typescriptle.d.ts
@@ -21,7 +21,7 @@ type Split<T extends string, Delimiter extends string = ""> =
     ? [Head, ...Split<Tail, Delimiter>]
     : // This check deals with empty strings to ensure that they do not end up in our list of
     // characters. `B` matches on an empty string, so this will match `'A'`, but not `''`
-    T extends `${infer A}${infer B}`
+    T extends `${infer A}${string}`
     ? [A]
     : [];
 
@@ -41,12 +41,194 @@ type Join<T extends string[]> = T extends [infer Head, ...infer Tail]
   ? `${CastToString<Head>}${Join<CastToStringList<Tail>>}`
   : "";
 
+// This type might look like it returns an object, but in fact it returns a list of all keys in the list
+// Example: ArrayKeyList<[true, true]> would return `['0', '1']`
+type ArrayKeyList<T extends unknown[]> = {
+  [P in keyof T]: P;
+};
+// This type turns a list into a union. This type is the backbone of our ability to compare numbers
+// Example: KeyUnion<['0', '1']> would return `'0' | '1'`
+type KeyUnion<T extends any[]> = ArrayKeyList<T>[number];
+
+// Converts a given number to a tuple of `Replacement`. This enables us to use numeric values when comparing tuples/lists
+// as we can use the resulting tuple can be used for conditional type checking.
+type NumberToTuple<
+  T extends number,
+  Replacement = true,
+  IsLengthInclusive extends boolean = false,
+  R extends Replacement[] = []
+> =
+  // if the length of built-up list has reached the number we are looking for
+  R["length"] extends T
+    ? // special case to increase the resulting length by 1. This is useful for cases where we use this type downstream and convert it to a union, which requires the actual number to be part of it.
+      // This is due to lists being 0 based, which would usually omit the actual number from our indexes
+      IsLengthInclusive extends true
+      ? [Replacement, ...R]
+      : R
+    : NumberToTuple<T, Replacement, IsLengthInclusive, [Replacement, ...R]>;
+
+// Checks whether `T` is greater or than or equal to `R`. This type works by checking whether the stringified version of `T`
+// extends the key union of the tuple version of `R`. Given that we convert our number `R` into a tuple of the same length as the number
+// we can check for inclusiveness in its key union to determine whether `T` is at least as large as `R`.
+// Note: this type does not work for negative numbers.
+type GreaterThanOrEqual<
+  T extends number,
+  R extends number
+> = `${T}` extends KeyUnion<NumberToTuple<R>> ? false : true;
+
+// Basically the same type as above, except that it enforces that `T` must be strictly larger than `R`.
+// It does so by using `NumberToTuple`'s `IsLengthInclusive` case, which will ensure that `R` is actually part
+// of the resulting union.
+// If the stringified version of `T` can be found in the union, then it is not grater than `R`.
+type GreaterThan<T extends number, R extends number> = `${T}` extends KeyUnion<
+  NumberToTuple<R, true, true>
+>
+  ? false
+  : true;
+
+// A utility type to make things more readable when computing lengths
+type Length<T extends { length: number }> = T["length"];
+
+// Utility type to check boolean intersections for trueness
+type IsTrue<T extends boolean> = [T] extends [never]
+  ? T extends false
+    ? false
+    : false
+  : T extends false
+  ? false
+  : true;
+
+// Boolean OR utility type
+type Or<T1, T2> = T1 extends true ? true : T2 extends true ? true : false;
+// Boolean AND utility type
+type And<T1, T2> = T1 extends true ? (T2 extends true ? true : false) : false;
+
+// This is a very crude type to allow iteration over tuples. It allows us to keep track of the current index
+// All it does is provide a linked list of numbers that will end in `OUT_OF_BOUNDS` when we are trying to iterate
+// outside of the number of supported values. This is a way to iterate finite lists. In our case we do not need
+// more than 5 values, so 0-4 will suffice. This type is loosely based on a similar type in `ts-toolbelt`.
+type Iteration = PossibleIterations[keyof PossibleIterations];
+type PossibleIterations = {
+  OUT_OF_BOUNDS: [-1, "OUT_OF_BOUNDS"];
+  0: [0, 1];
+  1: [1, 2];
+  2: [2, 3];
+  3: [3, 4];
+  4: [4, "OUT_OF_BOUNDS"];
+};
+
+// utility types to walk through the iteration list
+type IterationOf<T extends keyof PossibleIterations> = PossibleIterations[T];
+type IterationValue<T extends Iteration> = T[0];
+type NextIteration<T extends Iteration> = PossibleIterations[T[1]];
+
+// Given `Character` will find all instances of it in `T` and return `true[]` with a length
+// matching the number of times `Character` was found
+type GetAllCharacterOccurrences<Character extends string, T extends string[]> =
+  // If our list has elements in it
+  T extends [infer Head, ...infer Tail]
+    ? // and the currrent head matches our search character
+      Character extends Head
+      ? // add an entry to our return value and recurse
+        [true, ...GetAllCharacterOccurrences<Character, CastToStringList<Tail>>]
+      : // else just recurse
+        GetAllCharacterOccurrences<Character, CastToStringList<Tail>>
+    : // return an empty list to terminate recursion once we have exhausted the list
+      [];
+
+// Slices `List` from `IndexToStartAt` until the end
+type SliceStartingAt<
+  IndexToStartAt extends number,
+  List extends unknown[]
+> = List extends [...NumberToTuple<IndexToStartAt, any>, ...infer Rest]
+  ? Rest
+  : List;
+
+// Slices a list from index 0 to `IndexToSliceUpTo`.
+type SliceUpTo<
+  IndexToSliceUpTo extends number,
+  List extends unknown[],
+  PoppedList extends unknown[] = List,
+  Accumulator extends unknown[] = []
+> =
+  // Check if the list we have received contains at least as many items as the index we want to start slicing at
+  GreaterThanOrEqual<Length<List>, IndexToSliceUpTo> extends true
+    ? // If our `Accumulator` has reached the length of the index we want to slice up to
+      // we have successfully sliced off enough elements
+      GreaterThanOrEqual<Length<Accumulator>, IndexToSliceUpTo> extends true
+      ? Accumulator
+      : // Otherwise we check if we have more elements to slice and, if so, will
+      // keep recursing with a new entry added to `Accumulator`.
+      PoppedList extends [infer Head, ...infer Tail]
+      ? SliceUpTo<IndexToSliceUpTo, List, Tail, [...Accumulator, Head]>
+      : // We have no more elements to recurse over, terminate
+        []
+    : // If the list does not contain enough items, just return it as is
+      List;
+
+// Will return the number of characters in `List` which match `Character` after the index specified by our interation `I`
+type GetCharacterOccurrencesAfterIndex<
+  Character extends string,
+  List extends string[],
+  I extends Iteration
+> =
+  // guard against out of bounds iteration
+  IterationValue<NextIteration<I>> extends number & infer NextIterationValue
+    ? Length<
+        GetAllCharacterOccurrences<
+          Character,
+          CastToStringList<
+            SliceStartingAt<CastToNumber<NextIterationValue>, List>
+          >
+        >
+      >
+    : '"out of bounds, cannot compute"';
+
+// Will return the number of characters in `List` which match `Character` after the index specified by our interation `I`
+type GetCharacterOccurrencesBeforeIndex<
+  Character extends string,
+  List extends string[],
+  I extends Iteration
+> = Length<
+  GetAllCharacterOccurrences<
+    Character,
+    CastToStringList<SliceUpTo<IterationValue<I>, List>>
+  >
+>;
+
+// Computes `true[]` with the length of the number of times `Character` was matched at the same index in `T` and `R`.
+// Example: `GetExactmatches<'c', ['a','b','c','c','d'], ['c','a','c','c','e']>` would return `[true, true]` as we have two matches.
+type GetExactMatches<
+  Character extends string,
+  T extends string[],
+  R extends string[]
+> = T extends [infer Head, ...infer Tail]
+  ? R extends [infer RHead, ...infer RTail]
+    ? Character extends Head & RHead
+      ? [
+          true,
+          ...GetExactMatches<
+            Character,
+            CastToStringList<Tail>,
+            CastToStringList<RTail>
+          >
+        ]
+      : GetExactMatches<
+          Character,
+          CastToStringList<Tail>,
+          CastToStringList<RTail>
+        >
+    : []
+  : [];
+
 // These are just utility types to make the conditional types below more readable
 type CharacterNotFound<Character extends string> = ` ⬛ ${Character} `;
 type CharacterAtCorrectPosition<Character extends string> = ` 🟩 ${Character} `;
 type CharacterFound<Character extends string> = ` 🟨 ${Character} `;
 type EmptyWordPlaceholder = " ⬛    ⬛    ⬛    ⬛    ⬛  ";
 type InvalidWord = "       INVALID WORD        ";
+type InvalidCase =
+  "This is an invalid case and should never happen, please report a bug with steps to reproduce, thanks!";
 
 // This type will check for the existence of a character in a list of characters.
 // It does this by recursively checking each charater in the list `SplitWorkd` against our
@@ -59,11 +241,8 @@ type ContainsCharacter<
   ? // Character exists in solution, just not at the right spot
     CharacterToSearchFor extends CurrentCharacter
     ? CharacterFound<CharacterToSearchFor>
-    : RestOfWord extends string[]
-    ? // Recursively check the rest of the characters until we run out or find a match
-      ContainsCharacter<RestOfWord, CharacterToSearchFor>
-    : // Wrong character
-      CharacterNotFound<CharacterToSearchFor>
+    : // Recursively check the rest of the characters until we run out or find a match
+      ContainsCharacter<CastToStringList<RestOfWord>, CharacterToSearchFor>
   : // Wrong character
     CharacterNotFound<CharacterToSearchFor>;
 
@@ -78,50 +257,157 @@ type ValidWord<Word> = Word extends Words ? true : false;
 // either a `string` or `string[]`.
 type CastToStringList<T> = T extends string[] ? T : string[];
 type CastToString<T> = T extends string ? T : string;
+type CastToNumber<T> = T extends number ? T : number;
+
+// Utility type to compute counts required for our calculations.
+type ComputeCounts<
+  CurrentCharacter extends string,
+  FullSolution extends string[],
+  FullGuess extends string[],
+  I extends Iteration
+> = [
+  MatchingCharactersAfterCurrentIndexForSolution: CastToNumber<
+    GetCharacterOccurrencesAfterIndex<
+      CastToString<CurrentCharacter>,
+      FullSolution,
+      I
+    >
+  >,
+  MatchingCharactersBeforeCurrentIndexForSolution: CastToNumber<
+    GetCharacterOccurrencesBeforeIndex<
+      CastToString<CurrentCharacter>,
+      FullSolution,
+      I
+    >
+  >,
+  MatchingCharactersAfterCurrentIndexForGuess: CastToNumber<
+    GetCharacterOccurrencesAfterIndex<
+      CastToString<CurrentCharacter>,
+      FullGuess,
+      I
+    >
+  >,
+  MatchingCharactersBeforeCurrentIndexForGuess: CastToNumber<
+    GetCharacterOccurrencesBeforeIndex<
+      CastToString<CurrentCharacter>,
+      FullGuess,
+      I
+    >
+  >
+];
+
+type LowercaseString<T> = CastToString<Lowercase<T>>;
 
 // This type is our "entry" type for comparing two lists of characters.
-// `GuessCharacterList` are the characters of the word that we guessed
-// and `SolutionCharacterList` are the individual characters of the word we're looking for.
+// `CurrentGuessCharacterList` are the characters of the word that we guessed
+// and `CurrentSolutionCharacterList` are the individual characters of the word we're looking for.
 // Just as in our types above, we rely heavily on conditional types.
-type CompareWords<
-  GuessCharacterList extends string[],
-  SolutionCharacterList extends string[],
-  // The full list of characters for the solution which will remain untouched.
-  // This is needed for checking existence of a character _somewhere_ in the whole solution
-  FullSolutionCharacterList = SolutionCharacterList
-> =
-  // We need to have at least a list with one character to guess
-  GuessCharacterList extends [
-    infer CurrentGuessedLetter,
-    ...infer RestOfGuessedWord
-  ]
-    ? // We also need at least one character in the list of characters of our solution
-      SolutionCharacterList extends [
-        infer CurrentSolutionLetter,
-        ...infer RestOfSolution
+export type CompareWords<
+  CurrentGuessCharacterList extends string[],
+  CurrentSolutionCharacterList extends string[],
+  FullSolution extends string[] = CurrentSolutionCharacterList,
+  FullGuess extends string[] = CurrentGuessCharacterList,
+  CurrentIteration extends Iteration = IterationOf<0>,
+  // precompute values for the current iteration which are used in multiple branches
+  SlicedGuess = SliceStartingAt<IterationValue<CurrentIteration>, FullGuess>,
+  SlicedSolution = SliceStartingAt<
+    IterationValue<CurrentIteration>,
+    FullSolution
+  >,
+  NumberOfExactMatchesForCurrentGuessCharacter = Length<
+    GetExactMatches<
+      LowercaseString<CurrentGuessCharacterList[0]>,
+      CastToStringList<SlicedGuess>,
+      CastToStringList<SlicedSolution>
+    >
+  >
+> = [CurrentGuessCharacterList, CurrentSolutionCharacterList] extends [
+  [infer CurrentGuessCharacter, ...infer GuessTail],
+  [infer CurrentSolutionCharacter, ...infer SolutionTail]
+]
+  ? // exact match case
+    LowercaseString<CurrentGuessCharacter> extends CurrentSolutionCharacter
+    ? [
+        CharacterAtCorrectPosition<LowercaseString<CurrentGuessCharacter>>,
+        ...CompareWords<
+          CastToStringList<GuessTail>,
+          CastToStringList<SolutionTail>,
+          FullSolution,
+          FullGuess,
+          NextIteration<CurrentIteration>
+        >
       ]
-      ? [
-          // If both letters at the current position match, the guessed letter is correct
-          CurrentGuessedLetter extends CurrentSolutionLetter
-            ? CharacterAtCorrectPosition<CastToString<CurrentGuessedLetter>>
-            : // If they didn't match, the guessed letter might be used somewhere in the solution
-              ContainsCharacter<
-                CastToStringList<FullSolutionCharacterList>,
-                CastToString<CurrentGuessedLetter>
-              >,
-          // We call `CompareWords` recursively, with both the remaining guess characters and remaining
-          // solution characters. It is importat that we also explicitly pass in `FullSolutionCharacterList`
-          // so that it doesn't get reassigned to ` CastToStringList<RestOfSolution>` the next recursion.
-          // If that happend we would not be able to check the whole list for general existence of a character
-          // as the list would shrink with each recursion.
-          ...CompareWords<
-            CastToStringList<RestOfGuessedWord>,
-            CastToStringList<RestOfSolution>,
-            FullSolutionCharacterList
-          >
-        ]
-      : []
-    : [];
+    : // compute data for other cases
+    ComputeCounts<
+        LowercaseString<CurrentGuessCharacter>,
+        FullSolution,
+        FullGuess,
+        CurrentIteration
+      > extends [
+        infer SolutionCharactersAfter,
+        infer SolutionCharactersBefore,
+        infer GuessCharactersAfter,
+        infer GuessCharactersBefore
+      ]
+    ? [
+        // If our current character appeared in the solution before the current index
+        GreaterThan<SolutionCharactersBefore, 0> extends true
+          ? Or<
+              // If the number of times the character appeared in the solution is larger
+              // than the number of times it appeared in our guess
+              GreaterThan<SolutionCharactersBefore, GuessCharactersBefore>,
+              // or if the number of exact matches for this character
+              // after our current index is smaller than the number of times
+              // this character appears in the solution after the current index
+              // then we have a partial match
+              GreaterThan<
+                SolutionCharactersAfter,
+                NumberOfExactMatchesForCurrentGuessCharacter
+              >
+            > extends true
+            ? CharacterFound<LowercaseString<CurrentGuessCharacter>>
+            : // else the character couldn't be found
+              CharacterNotFound<LowercaseString<CurrentGuessCharacter>>
+          : // if the character appears somewhere in the solution after the current index
+          GreaterThan<SolutionCharactersAfter, 0> extends true
+          ? // First case: we have at least the same number of this character coming up in the guess as well as the solution
+            GreaterThanOrEqual<
+              SolutionCharactersAfter,
+              GuessCharactersAfter
+            > extends true
+            ? And<
+                // Ensure that we have more characters coming after than came before. This is necessary to account for characters
+                // which have already been "claimed" by previous occurrences of the current character
+                GreaterThan<SolutionCharactersAfter, GuessCharactersBefore>,
+                // Finally check if we have less exact matches coming up than available matching characters. If this is the case
+                // then we found a valid character
+                GreaterThan<
+                  SolutionCharactersAfter,
+                  NumberOfExactMatchesForCurrentGuessCharacter
+                >
+              > extends true
+              ? CharacterFound<LowercaseString<CurrentGuessCharacter>>
+              : CharacterNotFound<LowercaseString<CurrentGuessCharacter>>
+            : // Second case: We have less solution characters than guess characters. We check if the number of upcoming matches is less than the
+            // number of total valid characters. If that is the case, then this character is valid as a partial match.
+            GreaterThan<
+                SolutionCharactersAfter,
+                NumberOfExactMatchesForCurrentGuessCharacter
+              > extends true
+            ? CharacterFound<LowercaseString<CurrentGuessCharacter>>
+            : CharacterNotFound<LowercaseString<CurrentGuessCharacter>>
+          : CharacterNotFound<LowercaseString<CurrentGuessCharacter>>,
+        // recurse the rest of the guessed word
+        ...CompareWords<
+          CastToStringList<GuessTail>,
+          CastToStringList<SolutionTail>,
+          FullSolution,
+          FullGuess,
+          NextIteration<CurrentIteration>
+        >
+      ]
+    : InvalidCase
+  : [];
 
 // This wrapper type solely exists to hide the solution word from the expanded type that
 // TypeScript reports. The usage of a generic and conditional type prevents expansion.
---
+++ typescriptle.test-d.ts
@@ -0,0 +1,44 @@
+import { expectType } from "tsd";
+
+import { CompareWords } from "./typescriptle";
+
+// full match
+declare const T10: CompareWords<["a", "b", "c"], ["a", "b", "c"]>;
+expectType<[" 🟩 a ", " 🟩 b ", " 🟩 c "]>(T10);
+
+// full match, case insensitive
+declare const T20: CompareWords<["a", "B", "c"], ["a", "b", "c"]>;
+expectType<[" 🟩 a ", " 🟩 b ", " 🟩 c "]>(T20);
+
+// invalid character
+declare const T30: CompareWords<["a", "b", "c"], ["a", "x", "c"]>;
+expectType<[" 🟩 a ", " ⬛ b ", " 🟩 c "]>(T30);
+
+// same character appears multiple times in guess, once in solution, and has a direct match
+declare const T40: CompareWords<["a", "a", "c"], ["a", "b", "b"]>;
+expectType<[" 🟩 a ", " ⬛ a ", " ⬛ c "]>(T40);
+
+// same character appears multiple times in guess, once in solution, and has an indirect match
+declare const T50: CompareWords<["a", "a", "c"], ["b", "b", "a"]>;
+expectType<[" 🟨 a ", " ⬛ a ", " ⬛ c "]>(T50);
+
+// direct and indirect match, indirect match is last character in guess
+declare const T60: CompareWords<["a", "x", "a"], ["a", "a", "b"]>;
+expectType<[" 🟩 a ", " ⬛ x ", " 🟨 a "]>(T60);
+
+// direct and indirect match, indirect match has characters after it
+declare const T70: CompareWords<["a", "x", "a", "f"], ["a", "a", "b", "g"]>;
+expectType<[" 🟩 a ", " ⬛ x ", " 🟨 a ", " ⬛ f "]>(T70);
+
+// multiple indirect matches
+declare const T80: CompareWords<["x", "a", "a", "f"], ["a", "b", "b", "a"]>;
+expectType<[" ⬛ x ", " 🟨 a ", " 🟨 a ", " ⬛ f "]>(T80);
+
+// @ts-expect-error For some reason TS things the type instantiation here is possibly infinite.
+// We are getting the correct result type though, so for now let's mute this. It does not seem
+// to happen for all word combinations.
+declare const T90: CompareWords<
+  ["m", "o", "u", "t", "h"],
+  ["a", "c", "h", "e", "s"]
+>;
+expectType<[" ⬛ m ", " ⬛ o ", " ⬛ u ", " ⬛ t ", " 🟨 h "]>(T90);
